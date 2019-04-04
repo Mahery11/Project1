e97d51c6528f4886530afa003c76a9f775398d71
Thomas Maher <c3330366@uon.edu.au>
2019-04-04 05:55:37 +0000

    Added an alphabet loop that changes the values from ASCII into a string, these numbers can be checked by calling a function. However, I want the alphabet loop to be a function itself to clean up main().

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 //prototypes for all different cipher types
 /*int encryptRot(void);
@@ -7,34 +8,69 @@ int encryptSub(void);
 int decrpytSubgiven_all(void);
 int decryptRotgiven_cipher(void);
 int decryptSubgiven_cipher(void);*/
+int checkAlphabet(); //used to list each number and letter to their corresponding value
 
 int main() {
-    char alphabet[1] = {1}; //123 values
-    int pick = 0, startup = 1, x = 0;
+    char alphabet[61]; //starting the string for the alphabet
+    
+    for (char c = '0'; c <= ':'; c++) { //0=0, 1=1, ...
+         if (c > '9') { //applying a null factor, as not applying this creates unspecified values in the string
+            alphabet[c-48] = '\0';
+            //printf("%s\n", alphabet); 
+            break;
+        }
+        alphabet[c-48] = c; //assigning alphabet to its proper set number using ASCII numbers
+        //printf("%s\n", alphabet); //checking if the output is correct
+    }
+    for (char c = 'A'; c <= '['; c++) { //10=A, 11=B ... 35=Z
+        if (c > 'Z') { //applying a null factor, as not applying this creates unspecified values in the string
+            alphabet[c-55] = '\0';
+            //printf("%s\n", alphabet); 
+            break;
+        }        
+        alphabet[c-55] = c; //assigning alphabet to its proper set number using ASCII numbers
+        //printf("%s\n", alphabet); //checking if the output is correct
+    }
+    for (char c = 'a'; c <= '{'; c++) { //36=a, 37=b... 61=z
+        if (c > 'z') { //applying a null factor, as not applying this creates unspecified values in the string
+            alphabet[c-61] = '\0';
+            //printf("%s\n", alphabet); 
+            break;
+        }        
+        alphabet[c-61] = c; //assigning alphabet to its proper set number using ASCII numbers
+        //printf("%s\n", alphabet); //checking if the output is correct
+    }
+
+    checkAlphabet(alphabet); //if the values of the alphabet are unclear
+
+//NEXT TASK: CONVERT WRITTEN INPUT into identifiable/modifiable text
+//TURN THE ALPHABET INTO A FUNCTION SO IT IS NOT BLOCKED INTO THE MIDDLE OF MAIN
+
+    /*char x[];
+    int pick = 0, startup = 1;
     //while (startup == 1) { //(ensure the code exits when it is meant to)
         printf("What is the message you wish to encrypt/decrypt?\n");
-        scanf("%d", &x); //needs to be able to scan given letters (probably not an integer but will be a string)
+        scanf("%s", &x); //needs to be able to scan given letters (probably not an integer but will be a string)
         printf("Which of the following options do you wish to do with this message? (Select 1-n)\n"); //list printed over several lines, so it is easier to read
         printf("1. Rotation encryption\n2. Decryption of a rotation cipher, given the cipher text and rotation amount\n");
         printf("3. Substitution encryption\n4. Decryption of a substitution cipher, given the cipher text and rotation amount\n");
         printf("5. Decryption of a rotation cipher, given the cipher text\n6. Decryption of a substitution cipher, given the cipher text\n");
         printf("%s %d %d\n", alphabet, pick, startup); //this is to make the compiler stop complaining
-        scanf("%d", &pick);//deciding which cipher subject is to be used
+        scanf("%d", &pick);//deciding which cipher subject is to be used*/
         /*switch (pick){ 
-            case (1): encryptRot(x); //x is the value to be encrypted/decrypted
-            case (2): decryptRotgiven_all(x);
-            case (3): encryptSub(x);
-            case (4): decryptSubgiven_all(x);
-            case (5): decryptRotgiven_cipher(x);
-            case (6): decryptSubgiven_cipher(x);
-            default: //restart/reask question
+            case (1): encryptRot(x); break; //x is the value to be encrypted/decrypted
+            case (2): decryptRotgiven_all(x); break;
+            case (3): encryptSub(x); break;
+            case (4): decryptSubgiven_all(x); break;
+            case (5): decryptRotgiven_cipher(x); break;
+            case (6): decryptSubgiven_cipher(x); break;
+            default: printf("Option selected does not exist\nEnsure the option chosen is the number corresponding to the option desired"); continue;//restart/reask question, use continue or something else?
         }
     }*/
 
     return 0;
 }
 
-//ASCII alphabet, 0(48)-9(59), A(65)-Z(90), a(97)-z(122)
 //defintion of the cipher functions
 /*int encryptRot (void) { 
     
@@ -51,3 +87,10 @@ int encryptSub(void) {
 int decryptSubgiven_all(void) {
     
 }*/
+int checkAlphabet(char check[]) {
+    int i;
+    for (i=0; i<=61; i++) {
+        printf("%d %c\n", i, check[i]);
+    }
+    return 0;
+}
