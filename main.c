f4ec85e5a48316cecadfdf4fdb90018bc4653599
Thomas Maher <c3330366@uon.edu.au>
2019-04-11 06:25:11 +0000

    Changed the rotation encryption The message to be encrypted is now recorded before the function is called and is also returned to main so it can be manipulated further if need be. Additionally, the original message can be stored by making a different string equal to the original input string. Made a start to the the substitution encryption

--- main.c
+++ main.c
@@ -2,60 +2,74 @@
 #include <string.h>
 
 //prototypes for all different cipher types
-/*void encryptRot(void);
-int decryptRotgiven_all(void); 
-int encryptSub(void);
-int decrpytSubgiven_all(void);
-int decryptRotgiven_cipher(void);
-int decryptSubgiven_cipher(void);*/
+char encryptRot(char *input);
+//int decryptRotgiven_all(void); 
+char encryptSub(char *input);
+//int decrpytSubgiven_all(void);
+//int decryptRotgiven_cipher(void);
+//int decryptSubgiven_cipher(void);*/
 
 int main() {
 //NEXT TASK: CONVERT WRITTEN INPUT into identifiable/modifiable text
 //TURN THE ALPHABET INTO A FUNCTION SO IT IS NOT BLOCKED INTO THE MIDDLE OF MAIN
 
-    //char x[];
+    char input[50] = {};
     int pick = 0;
-    //printf("What is the message you wish to encrypt/decrypt?\n");
-    //scanf("%s", &x); //needs to be able to scan given letters (probably not an integer but will be a string), may need to be implemented into each function
+    printf("What is the message you wish to encrypt/decrypt?\n");
+    scanf("%s", input); //needs to be able to scan given letters (probably not an integer but will be a string), may need to be implemented into each function
+    int fail=1;
+    while (fail==1) {
     printf("Which of the following options do you wish to do with this message? (Select 1-n)\n"); //list printed over several lines, so it is easier to read
     printf("1. Rotation encryption\n2. Decryption of a rotation cipher, given the cipher text and rotation amount\n");
     printf("3. Substitution encryption\n4. Decryption of a substitution cipher, given the cipher text and rotation amount\n");
     printf("5. Decryption of a rotation cipher, given the cipher text\n6. Decryption of a substitution cipher, given the cipher text\n");
-    printf("%s %d %d\n", alphabet, pick, startup); //this is to make the compiler stop complaining
     scanf("%d", &pick);//deciding which cipher subject is to be used*/
     switch (pick){ 
-        case (1): encryptRot(); break;
-        //case (2): decryptRotgiven_all(x); break;
-        //case (3): encryptSub(x); break;
-        //case (4): decryptSubgiven_all(x); break;
-        //case (5): decryptRotgiven_cipher(x); break;
-        //case (6): decryptSubgiven_cipher(x); break;
-        default: printf("Option selected does not exist\nEnsure the option chosen is the number corresponding to the option desired"); continue;//restart/reask question, use continue or something else?
+        case (1): encryptRot(input); fail=0; break; //calls the rotation encryption function and reassigns input to the encryption
+        //case (2): decryptRotgiven_all(input); fail=0; break;
+        case (3): encryptSub(input); fail=0; break;
+        //case (4): decryptSubgiven_all(input); fail=0; break;
+        //case (5): decryptRotgiven_cipher(input); fail=0; break;
+        //case (6): decryptSubgiven_cipher(input); fail=0; break;
+        default: printf("Option selected does not exist\nEnsure the option chosen is the number corresponding to the option desired\n"); fail = 1;
+        }
     }
+    printf("%s\n", input);
     return 0;
 }
 
 //defintion of the cipher functions
-void encryptRot (void) { 
-    char input[200] = {};
-    scanf("%s", input);
-    int i;
-    for (i=0; input[]; i++) {
-        input[i] = (char)input[i] + 2;
-        if (input[i] > 90)
-            input[i] = (char)(input[i]) - 26;
-    }
-    printf("%s\n", input);
+char encryptRot (char *input) { //is functioning - CANNOT READ WHITE SPACE CURRENTLY
+    //char input[200] = {}; //defining a string for the initial message
+    int i, n;
+    //printf("This is rotation encryption.\nEnter the message you wish to encrypt: \n");
+    //scanf("%s", input); //reading the input from the user until a white space
+    printf("What is the rotation number for this cipher?\n");
+    scanf("%d", &n); //n = rotation key scale
+    if (n!=0) {
+    for (i=0; input[i]; i++) {
+            input[i] = (char)input[i] + n; //for whatever rotation keys, the function will add/substitute each element of the string by what rotation key scale has been given
+            if (input[i] > 90)
+                input[i] = (char)(input[i]) - 26; //if the encryption rotation exceeds the ASCII limit, this will allow it to return to our range of 65 - 90 characters
+            if (input[i] < 65)
+                input[i] = (char)(input[i]) + 26; //if the encryption rotation is below the ASCII limit, this will allow it to return to our range of 65 - 90 characters  
+            }
+        }
+    else if (n==0) //if the rotation number is 0, nothing will change, so it skips reassignment of the string
+        printf("Since the rotation number is 0, the message will not change\n");
+    printf("Encryption key: %d\n", n);
+    return *input; //reassigns the string input to the main function which can be freely manipulated
 }
 /*
 int decryptRotgiven_all(void) {
     
-}
+}*/
 
-int encryptSub(void) {
-    
+char encryptSub(char *input) {
+    int i, n;
+    return 0;
 }
-
+/*
 int decryptSubgiven_all(void) {
     
 }*/
