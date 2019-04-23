3edb03bd7def0f95b61ae261309727bb958b31c3
Thomas Maher <thomas831_lives@hotmail.com>
2019-04-23 15:50:33 +0000

    Serious alterations made.
    Final output now contains the original message next to the ciphered message as well as the key of the cipher compared to a regular alphabet (no scale given for rotation ciphers [yet?])
    
    Added a nearly 'functional' rotation cipher decryption function when the cipher is given and currently only fails when white space exists in the entered message that needs to be decrypted and currently skips over this element. Which is the main bug to currently fix.
    There are also some other minor bugs to look at.
    
    Other decrypting functions have been theorised and will be ready to coded soon, especially the substitution ciphers and even the rotation cipher without the key.

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void decryptRotgiven_all(char *str_adj, char *alpha, char *key);
 char encryptSub(char *str_adj, char *alpha);
 //int decrpytSubgiven_all(void);
 //int decryptRotgiven_text(void);
-//int decryptSubgiven_text(void);*/
+//int decryptSubgiven_text(void);
 void Alphabet_f(char *alpha);
 
 int main() {
@@ -15,18 +15,13 @@ int main() {
 //TURN THE ALPHABET INTO A FUNCTION SO IT IS NOT BLOCKED INTO THE MIDDLE OF MAIN
     char alpha[26]; //creates a string for the basic alphabet string to exist
     char key[26]; //EXISTS AS SOMETHING TO BE COMPARED TO THE ALPHABET FUNCTION TO SHOW THE KEY
-    char str_adj[50] = "THIS is a MEsSage with WhiTe SpaCES"; //CURRENTLY HARD CODED AND MUST BE REPLACED BY FILE I/O
-    for (int i=0; str_adj[i]; i++) {
-        if (str_adj[i]>=97 && str_adj[i]<=122) //checking for lowercase
-            str_adj[i] = str_adj[i] - 32; //fixing lowercase
-        if (str_adj[i]<65 || str_adj[i]>122) //checks if it is not within the ASCII range and does not change it if is
-            str_adj[i] = str_adj[i];
-    }
+    char str_adj[50] = "THIS is a MEsSage with WhiTe SpaCEs"; //CURRENTLY HARD CODED AND MUST BE REPLACED BY FILE I/O
     char origin_mes[50];
     int pick = 0;
-    for (int i=0; str_adj[i]; i++) {
-        origin_mes[i] = str_adj[i];
-    }
+    for (int i=0; str_adj[i]; i++) { //retains the original message and it's respective characters
+        origin_mes[i] = str_adj[i]; //Need to apply a null factor at the end of this, for some reason capital letters are unaffected and this string prints out normally
+                                    //However if it finishes with a lower case it is followed by an @ symbol in the final output.
+        }
     Alphabet_f(alpha); //assigns a letter from A-Z to each element of the string AND FIXES ANY ANOMALIES IN THE MESSAGE (may implement later for shorter functions)
     //printf("What is the message you wish to encrypt/decrypt?\n");
     //scanf("%s", str_adj); //replace with File I/O to read white space
@@ -47,7 +42,7 @@ int main() {
         default: printf("Option selected does not exist\nEnsure the option chosen is the number corresponding to the option desired\n"); fail = 1;
         }
     }
-    printf("Original message: %s\nCiphered message: %s\n", origin_mes, str_adj);
+    printf("Entered message:  %s\nCiphered message: %s\n", origin_mes, str_adj);
     printf("Encryption key: \n%s\n%s\n", alpha, key);
     return 0;
 }
@@ -60,13 +55,19 @@ void encryptRot (char *str_adj, char *alpha, char *key) { //is functioning - CAN
     scanf("%d", &n); //n = rotation key scale
     if (n!=0) {
     for (i=0; str_adj[i]; i++) {
+            if (str_adj[i]>=97 && str_adj[i]<=122) //checking for lowercase
+                str_adj[i] = str_adj[i] - 32; //fixing lowercase
+            if (str_adj[i]<65 || str_adj[i]>122) //checks if it is not within the ASCII range and does not change it if is
+                str_adj[i] = str_adj[i];
+            else { //if it is in the correct range it will continue as normal.
             str_adj[i] = (char)str_adj[i] + n; //for whatever rotation keys, the function will add/substitute each element of the string by what rotation key scale has been given
             if (str_adj[i] > 90)
                 str_adj[i] = (char)(str_adj[i]) - 26; //if the encryption rotation exceeds the ASCII limit, this will allow it to return to our range of 65 - 90 characters
-            if (str_adj[i] < 65)
+            if (str_adj[i] < 65) //UNSURE: Why are the white spaces not being affected by this?
                 str_adj[i] = (char)(str_adj[i]) + 26; //if the encryption rotation is below the ASCII limit, this will allow it to return to our range of 65 - 90 characters  
                 }
             }
+        }
     else if (n==0) //if the rotation number is 0, nothing will change, so it skips reassignment of the string
     printf("Since the rotation number is 0, the message will not change\n");
     for (i=0; alpha[i]; i++) { //creates a string that is the key of the cipher which can be clearly observed in the final output
@@ -78,35 +79,39 @@ void encryptRot (char *str_adj, char *alpha, char *key) { //is functioning - CAN
     }
 }
 
-void decryptRotgiven_all(char *str_adj, char *alpha, char *key) {
-    int i, k, n;
+void decryptRotgiven_all(char *str_adj, char *alpha, char *key) { //THERE IS A LOT OF BUGS IN THIS CODE
+//WHITE SPACE IS STILL BEING CHANGED UNLIKE THE ENCRYPTION
+//Scaling works correctly, now it is simply accounting for white space
+    int i, k, n; //declaring multiple variables that will be used for different things
     printf("What is the key of this cipher?\n1. Determine the scale with a number\n2. Enter the exact key (all caps, no spaces)\n");
-    scanf("%d", &k);
-    if (k == 1) {
+    scanf("%d", &k); //scanned for an option as to how the cipher will be entered
+    if (k == 1) { //option 1
         printf("What is the rotation number for this cipher? (Scale exists between -25 and 25)\n");
-        scanf("%d", &n);
-        //if ((n>25) || (n<-25)
-        n = n % 26;
+        scanf("%d", &n); //the user provides an integer of the rotation number that the cipher works under
+        n = -(n % 26); //this number is a negative as the scale will have to work in reverse, it has a modulus in case the number exceeds the given domain
         for (i=0; alpha[i]; i++) { //creates a string that is the key of the cipher which can be clearly observed in the final output
-            key[i] = alpha[i] + n;
+            key[i] = alpha[i] - n; //creating the cipher key since the exact key has not been given, just the rotation number
             if (key[i] > 90)
                 key[i] = (char)(key[i]) - 26; //if the encryption rotation exceeds the ASCII limit, this will allow it to return to our range of 65 - 90 characters
             if (key[i] < 65)
                 key[i] = (char)(key[i]) + 26; //if the encryption rotation is below the ASCII limit, this will allow it to return to our range of 65 - 90 characters  
             }
     }
-    if (k == 2) {
+    if (k == 2) { //if the user inputs the exact key with no spaces in all caps, it can be directly transferred into the string and be used
         printf("Enter the exact key of this cipher: ");
-        scanf("%s", key);
+        scanf("%s", key); //no & needed as this will apply the input directly into the string - which also doesn't need to worry about white space since it is one line
     }
     for (i=0; str_adj[i]; i++) {
         if (k==1) { //checks if a rotation number has been given or if it must be calculated
-            str_adj[i] = str_adj[i] + n;
+            //a filter for white space needs to be added here - also unsure if they are even elements that even exist.
+            str_adj[i] = str_adj[i] + n; //STILL NEEDS TO ACCOUNT FOR WHITE SPACE, this adjusts the string according to the rotation number (which will usually be 
+            // changed in sign as it must have the reverse effect of a rotation encryption)
         }
-    else {
-        n = (char)key[0] - (char)alpha[0];    
-        i--;
-        k = 1;
+        else { //if the rotation number has not been given, it must be calculated by finding the difference between the cipher string and the regular alphabet string
+               //using ASCII numbers
+            n = -((char)key[0] - (char)alpha[0]); //this number is a negative as the scale will have to work in reverse
+            i--; //since a this has been one loop, the increment must be cancelled with a decrement
+            k = 1; //now that the rotation number is defined/has been found, the string can be decrypted
         }
     }
 }
